23f6457a5d23fe273cf9d54432b723095b55ce89
Leonid Latyshev <forlemonster@gmail.com>
2023-07-10 11:42:45 +1000

    Initial Commit

---
+++ Friends/AppDelegate.h
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.h
+//  Friends
+//
+//  Created by Яна Латышева on 10.07.2023.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
---
+++ Friends/ViewController.h
@@ -0,0 +1,14 @@
+//
+//  ViewController.h
+//  Friends
+//
+//  Created by Яна Латышева on 10.07.2023.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
